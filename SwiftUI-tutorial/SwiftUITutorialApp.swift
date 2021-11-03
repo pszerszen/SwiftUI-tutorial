21b5922c179205cbbeb2d4688dfff3b6cf86426b
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-03 12:15:43 +0100

    initial commit

---
+++ SwiftUI-tutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorialApp.swift
+//  SwiftUI-tutorial
+//
+//  Created by Piotr Szerszeń on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
